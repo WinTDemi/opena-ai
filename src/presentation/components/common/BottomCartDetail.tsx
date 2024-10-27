bfa07cdf182ad124000ceb0302a7e5f723f3617d
WinTDemi <120547068+WinTDemi@users.noreply.github.com>
2024-10-27 16:12:57 +0700

    Fix total DetailPage

--- src/presentation/components/common/BottomCartDetail.tsx
+++ src/presentation/components/common/BottomCartDetail.tsx
@@ -8,7 +8,7 @@ function BottomCartDetail({ amount, price }: BottomCartDetailProps) {
         <div className="absolute bottom-0 flex items-center justify-between w-full h-24 p-2 bg-white">
             <div className="w-fit">
                 <p>Total price:</p>
-                <p>${price ? (amount * price) : 0}</p>
+                <p>${price ? (amount * price).toFixed(2) : "0.00"}</p>
             </div>
             <button className="w-3/5 text-white rounded-3xl btn bg-green-common">Add to Cart</button>
         </div>
